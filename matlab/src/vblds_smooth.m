46b9ad37b993ad099b8c99237c4993723d29f497
jundsp <43224513+jundsp@users.noreply.github.com>
2021-01-30 00:28:38 -0800

    Update vblds_smooth.m

--- matlab/src/vblds_smooth.m
+++ matlab/src/vblds_smooth.m
@@ -13,7 +13,7 @@
 
 function [mu_hat,V_hat, V12] = vblds_smooth(mu_plus,V_plus,mu,V,A,Q,Sigma_AQA)
 
-	G = woodbury_inversion(V,Sigma_AQA);
+    G = woodbury_inversion(V,Sigma_AQA);
     m = A*G*mu;
     P = A*G*V*A' + Q;
     
